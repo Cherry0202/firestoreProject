e588d5ceba3999e7ab90260938e4897b6d6715ab
Cherry <ex00ia2432@gmail.com>
2019-12-12 19:21:13 +0900

    [clean]動くところまで

--- main.go
+++ main.go
@@ -37,6 +37,7 @@ func main() {
 		fmt.Println(err)
 	}
 
+	wg := sync.WaitGroup{}
 	raw, err := ioutil.ReadFile("./products.json")
 
 	if err != nil {
@@ -49,42 +50,23 @@ func main() {
 	json.Unmarshal(raw, &product)
 
 	batch := client.Batch()
-	//Ref := client.Collection("Products").Doc("Mac")
 	start := time.Now()
-	fmt.Println("スタート！！")
-	// for _, p := range product {
-	// 	batch.Set(Ref, p)
-	// }
-	wg := sync.WaitGroup{}
+	fmt.Println("スタート")
 	for num, p := range product {
-		//fmt.Println(product[num].Title)
-		//fmt.Println(product[num].Price)
-		//fmt.Println(product[num].Url)
-
-		//Ref := client.Collection("Products3").Doc(strconv.Itoa(num))
-		//_, err = Ref.Set(ctx, product[num])
 		//go SetBatch(wg, batch, client, p, num)
 		SetBatch(wg, batch, client, p, num)
 	}
 	wg.Wait()
 	_, err = batch.Commit(ctx)
 	if err != nil {
-		// Handle any errors in an appropriate way, such as returning them.
 		log.Printf("An error has occurred: %s", err)
 	}
 	end := time.Now()
 	fmt.Printf("かかった時間：%f秒\n", (end.Sub(start)).Seconds())
-
-	defer func() {
-		err := recover()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
 }
 
 func SetBatch(wg sync.WaitGroup, batch *firestore.WriteBatch, client *firestore.Client, p Product, num int) {
 	//defer wg.Done()
-	Ref := client.Collection("Products5").Doc(strconv.Itoa(num))
+	Ref := client.Collection("Product6").Doc(strconv.Itoa(num))
 	batch.Set(Ref, p)
 }
